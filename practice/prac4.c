854898969ec4fd3a493f32842bbfdb86c3d8c262
acrostm <zjc971109@gmail.com>
2023-11-08 01:18:05 -0800

    set up for prac 4

---
+++ practice/prac4.c
@@ -0,0 +1,12 @@
+/* Practice set 4 Pipe and FIFO */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <string.h>
+
+
+int main(int argc, char** argv)
+{
+    
+}
